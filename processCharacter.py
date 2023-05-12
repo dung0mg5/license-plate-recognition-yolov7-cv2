8ebaa3829bc1d188338fe595673d435f0eae3a03
Hao <hideonbush8405@gmail.com>
2023-05-12 18:11:44 +0700

    done project

---
+++ processCharacter.py
@@ -0,0 +1,14 @@
+import re
+def get_valid_chars(input_str):
+    valid_chars = ""
+    for char in input_str:
+        if char.isalnum() or char in ['-', '.']:
+            valid_chars += char
+    return valid_chars
+
+
+plate_pattern = r'^[0-9]{2}[A-Z]{1}-?[0-9]{3}\.?[0-9]{2}$'
+
+
+def is_valid_license_plate(plate):
+    return bool(re.match(plate_pattern, plate))
